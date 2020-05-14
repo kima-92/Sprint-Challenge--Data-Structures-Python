fb7a7916e2bbbdddd45cb1936c4ada98ddd3a62a
Wilma <kima_92@fastmail.com>
2020-05-14 19:12:19 -0400

    Bugfixed the append method. Now all tests pass

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,19 +5,27 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.storage = DoublyLinkedList()
+        self.current_node = None
 
     def append(self, item):
+
+        # If current_node is None
+        if self.current_node == None:
+            # Set current_node to store the head of storage 
+            self.current_node = self.storage.head
+
         # If storage.len is less than capacity
         if self.storage.length < self.capacity:
-            # Add item to head of storage
-            self.storage.add_to_head(item)
-
-        # Else: If storage.len is equal than capacity
+            # Add item to tail of storage
+            self.storage.add_to_tail(item)
+        
+        # Else:
         else:
-            # Remove from tail of storage
-            self.storage.remove_from_tail()
-            # Add item to head
-            self.storage.add_to_head(item)
+            # Replaze current_node's value with this item
+            self.current_node.value = item
+            # Set current_node to current_node.next
+            # To save the next item in the next node
+            self.current_node = self.current_node.next
 
     def get(self):
         # If storage.length is 0
@@ -29,15 +37,15 @@ class RingBuffer:
         else:
             # Create a list to save all the values
             my_list = []
-            # Set current_node to storage.head
-            current_node = self.storage.head
+            # Set current to storage.head
+            current = self.storage.head
 
-            # While current_node is not empty
-            while current_node != None:
-                # append current_node to storage
-                my_list.append(current_node.value)
-                # set current_node to current_node.next
-                current_node = current_node.next
+            # While current is not empty
+            while current != None:
+                # append current to storage
+                my_list.append(current.value)
+                # set current to current.next
+                current = current.next
 
             # Return the list
             return my_list
