a73a7dae811b59da0359ab681a6ce2a00e111048
Wilma <kima_92@fastmail.com>
2020-05-15 10:14:56 -0400

    Imported DoublyLinkedList to names.py

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from doubly_linked_list import DoublyLinkedList
 
 start_time = time.time()
 
@@ -12,15 +13,26 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+"""
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
 
+The runtime was:
+10.852504014968872 seconds
+"""
+
+
+
+
+
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
